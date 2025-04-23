ce0168149eea29ce1c299b378c5cd1a264b665c6
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2025-04-23 11:02:36 -0300

    feat: command menu

---
+++ modules/menu.py
@@ -0,0 +1,15 @@
+def show_menu():
+    print("---" * 6 + "  Menu  " + "---" * 6)
+    print(
+        " "
+        "1) Inserir número\n",
+        "2) Buscar número\n",
+        "3) Deletar número\n",
+        "4) Mostrar caminho Pré-Ordem\n",
+        "5) Mostrar caminho Em Ordem\n",
+        "6) Mostrar caminho Pós-Ordem\n",
+        "7) Mostrar altura\n",
+        "8) Mostrar balanceamento da árvore\n",
+        "9) Mostrar árvore\n",
+        "0) Sair",
+    )
